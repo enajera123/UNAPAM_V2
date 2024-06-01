8c7de9638c6ad5fd96fb78ca5f24245443b75b77
Daniel Granados <granadosdaniel566@gmail.com>
2024-05-31 21:23:17 -0600

    [ADD] firebase upload file method

--- src/firebase/fileMethod.ts
+++ src/firebase/fileMethod.ts
@@ -1,5 +1,6 @@
 import { UploadMetadata, deleteObject, getDownloadURL, listAll, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '@/firebase/config';
+import firebase from 'firebase/compat/app';
 
 
 export const upload_image=async(file:string,image_extension:string,path:string)=>{
@@ -19,4 +20,21 @@ export const upload_image=async(file:string,image_extension:string,path:string)=
 export const delete_image_firebase = async(path:string)=>{
     const storageRef = ref(storage, path)
     await deleteObject(storageRef)
+}
+
+export const upload_file= async (file:Uint8Array,file_type:string,path:string)=>{
+    const storageRef = firebase.storage().ref(`${path}`);
+    const metadata = {
+      contentType: file_type
+    };
+  
+    return await storageRef.put(file, metadata)
+      .then(() => {
+        console.log('Archivo subido correctamente!');
+        return true
+      })
+      .catch((error) => {
+        console.error('Error al subir el archivo:', error);
+        return false
+      });
 }
