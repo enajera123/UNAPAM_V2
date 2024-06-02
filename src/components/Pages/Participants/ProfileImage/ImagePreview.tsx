a58d7c909f1a79e9ced041ac76d334761b1edcfc
Daniel Granados <granadosdaniel566@gmail.com>
2024-06-01 20:33:47 -0600

    [FIX] image preview

--- src/components/Pages/Participants/ProfileImage/ImagePreview.tsx
+++ src/components/Pages/Participants/ProfileImage/ImagePreview.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import Image from "next/image"
 
 const ImagePreview = ({link,show,setShow}:{link:string,show:boolean,setShow:()=>void}) => {
+
+  const handleKeyDown =(e:React.KeyboardEvent<HTMLDivElement>)=>{
+    if(e.key==='Escape'){
+      setShow()
+    }
+    console.log(e.key)
+  }
   return !show ? null : (
-    <div className='flex flex-col items-center justify-center h-screen absolute w-full bg-black/50 top-0 left-0 z-50 backdrop-blur-xl' onClick={setShow}>
-        <Image src={link} alt='' width={1024} height={1024} className={`bg-red-200 rounded-full h-3/4 aspect-auto object-contain z-20 hover:brightness-110 `} onClick={(e)=>e.stopPropagation()} />
+    <div onKeyDown={handleKeyDown} className='flex flex-col items-center justify-center h-screen absolute w-full bg-black/50 top-0 left-0 z-50 backdrop-blur-xl' onClick={setShow}>
+        <Image onKeyDown={handleKeyDown} src={link} alt='' width={550} height={550} className={` h-3/4  aspect-square  object-contain z-20 hover:brightness-110 `} onClick={(e)=>e.stopPropagation()} />
     </div>
   )
 }
