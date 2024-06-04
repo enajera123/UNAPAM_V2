86b9aa61d7a38d3b88a57310c9e17b7a2a2ef61f
Daniel Granados <granadosdaniel566@gmail.com>
2024-06-03 21:02:52 -0600

    [ADD] max length to yup validation

--- src/validation/YupHealth.ts
+++ src/validation/YupHealth.ts
@@ -3,25 +3,25 @@ import * as Yup from 'yup';
 export const validationSchemeHealth = Yup.object().shape({
     bloodType: Yup.string().required("El tipo de sangre es requerido"),
     participantDisseases:Yup.array().of(Yup.object().shape({
-        disease: Yup.string().required("Campo requerido"),
-        description:Yup.string().required("Campo requerido")
+        disease: Yup.string().required("Campo requerido").max(191),
+        description:Yup.string().required("Campo requerido").max(191)
     })),
     participantMedicines:Yup.array().of(Yup.object().shape({
-        medicine: Yup.string().required("Campo requerido"),
-        description: Yup.string().required("Campo requerido"),
+        medicine: Yup.string().required("Campo requerido").max(191),
+        description: Yup.string().required("Campo requerido").max(191),
     })),
     contactOne: Yup.object().shape({
-      firstName: Yup.string().required("Campo requerido"),
-      firstSurname: Yup.string().required("Campo requerido"),
-      secondSurname: Yup.string().required("Campo requerido"),
-      phoneNumber: Yup.string().required("Campo requerido"),
-      relationship: Yup.string().required("Campo requerido")
+      firstName: Yup.string().required("Campo requerido").max(191),
+      firstSurname: Yup.string().required("Campo requerido").max(191),
+      secondSurname: Yup.string().required("Campo requerido").max(191),
+      phoneNumber: Yup.string().required("Campo requerido").max(191),
+      relationship: Yup.string().required("Campo requerido").max(191)
     }),
     contactTwo: Yup.object().shape({
-        firstName: Yup.string().required("Campo requerido"),
-        firstSurname: Yup.string().required("Campo requerido"),
-        secondSurname: Yup.string().required("Campo requerido"),
-        phoneNumber: Yup.string().required("Campo requerido"),
-        relationship: Yup.string().required("Campo requerido")
+        firstName: Yup.string().required("Campo requerido").max(191),
+        firstSurname: Yup.string().required("Campo requerido").max(191),
+        secondSurname: Yup.string().required("Campo requerido").max(191),
+        phoneNumber: Yup.string().required("Campo requerido").max(191),
+        relationship: Yup.string().required("Campo requerido").max(191)
     }),
 })
