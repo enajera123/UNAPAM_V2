da09ae5da2af449eec7e655a2ce8b8c9a56e5929
enajera <estebannajera42@gmail.com>
2024-06-10 13:34:12 -0600

    fix: number course

--- src/components/Pages/Courses/validateCourseFields.ts
+++ src/components/Pages/Courses/validateCourseFields.ts
@@ -16,7 +16,6 @@ export async function validateFields(
         fnDate: !fnDate ? "Por favor, ingresa una fecha de finalización." : null,
         location: !location ? "Por favor, ingresa una locación para el curso." : null,
         description: !description ? "Por favor, ingresa una descripción." : null,
-        courseNumber: !courseNumber || isNaN(Number(courseNumber)) ? "Por favor, ingresa un número de curso válido." : null,
         name: !name ? "Por favor, ingresa un nombre." : null,
         quota: !quota || isNaN(Number(quota)) ? "Por favor, ingresa un cupo válido." : null,
         teacher: !teacher || /\d/.test(teacher) ? "Por favor, ingresa un nombre de profesor válido." : null,
