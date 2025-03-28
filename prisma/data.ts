95aaf8b76e8479ef7de93035a0c22adc6cf308c5
enajera123 <estebannajera42@gmail.com>
2025-03-28 08:46:08 -0600

    fix fast

---
+++ prisma/data.ts
@@ -0,0 +1,94 @@
+export const data = {
+    cursos: [
+        {
+            name: "Explorando Costa Rica: desde nuestra identidad cultural G1",
+            courseNumber: "UPPZ009",
+            description: "Lunes de 9 a 11:30 am",
+            location: "D003",
+            professor: "Jorge Arias Chaves",
+            intialDate: "31-03-2025",
+            finalDate: "02-06-2025"
+        },
+        {
+            name: "Explorando Costa Rica: desde nuestra identidad cultural G2",
+            courseNumber: "UPPZ010",
+            description: "Jueves de 9 a 11:30 am",
+            location: "Aprendizaje activo",
+            professor: "Jorge Arias Chaves",
+            intialDate: "03-04-2025",
+            finalDate: "05-06-2025"
+        },
+        {
+            name: "Inglés básico G1",
+            courseNumber: "UPPZ011",
+            description: "Lunes de 1 a 3:30 pm",
+            location: "L001",
+            professor: "Brenda Arias Calderón",
+            intialDate: "31-03-2025",
+            finalDate: "02-06-2025"
+        },
+        {
+            name: "Inglés básico G2",
+            courseNumber: "UPPZ012",
+            description: "Lunes de 9 a 11:30 am",
+            location: "L003",
+            professor: "Brenda Arias Calderón",
+            intialDate: "31-03-2025",
+            finalDate: "02-06-2025"
+        },
+        {
+            name: "Inglés básico G3",
+            courseNumber: "UPPZ013",
+            description: "Lunes de 9 a 11:30 am",
+            location: "A006",
+            professor: "Plocation Romero Elizondo",
+            intialDate: "31-03-2025",
+            finalDate: "02-06-2025"
+        },
+        {
+            name: "Uso de Canva 2",
+            courseNumber: "UPPZ014",
+            description: "Martes de 1 a 3 pm",
+            location: "L001",
+            professor: "Esteban Nájera Morales",
+            intialDate: "01-04-2025",
+            finalDate: "03-06-2025"
+        },
+        {
+            name: "Uso del teléfono inteligente 2",
+            courseNumber: "UPPZ015",
+            description: "Martes de 3 a 5 pm",
+            location: "L001",
+            professor: "Esteban Granados Sibaja",
+            intialDate: "01-04-2025",
+            finalDate: "03-06-2025"
+        },
+        {
+            name: "Huertos Orgánicos: Cultivando Salud desde casa",
+            courseNumber: "UPPZ016",
+            description: "Martes de 9 a 11:30 am",
+            location: "L001",
+            professor: "Farith Delgado Vargas",
+            intialDate: "01-04-2025",
+            finalDate: "03-06-2025"
+        },
+        {
+            name: "Computación básica G1",
+            courseNumber: "UPPZ017",
+            description: "Viernes de 1 a 3 pm",
+            location: "L001",
+            professor: "Dania Quirós Zúñiga",
+            intialDate: "04-04-2025",
+            finalDate: "06-06-2025"
+        },
+        {
+            name: "Computación básica G2",
+            courseNumber: "UPPZ018",
+            description: "Viernes de 3 a 5 pm",
+            location: "L001",
+            professor: "Dania Quirós Zúñiga",
+            intialDate: "04-04-2025",
+            finalDate: "06-06-2025"
+        }
+    ]
+};
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,55 +1,107 @@
 import { faker } from "@faker-js/faker"
 import { Grade, PrismaClient, State, StateParticipantOnCourse, TypeIdentification, YesOrNo } from "@prisma/client";
+import * as fs from "fs/promises";
+import * as path from "path";
+import { data } from "./data";
 const prisma = new PrismaClient();
+// import data from "./data.json" assert { type: "json" };
+async function seed() {
+    try {
+        // const filePath = "data.json"
+        // const fileContent = await fs.readFile(filePath, "utf-8");
+        // const data = JSON.parse(fileContent);
+        console.log(data)
+        for (const course of data.cursos) {
+            await prisma.course.create({
+                data: {
+                    courseNumber: course.courseNumber,
+                    name: course.name,
+                    description: course.description,
+                    location: course.location,
+                    professor: course.professor,
+                    quota: 12,
+                    needMedicalReport: YesOrNo.No,
+                    state: State.Active,
+                    initialDate: course.intialDate,
+                    finalDate: course.finalDate,
+
+                    // initialDate: new Date(coourse.intialDate),
+                    // finalDate: new Date(coourse.finalDate),
+                }
+            })
+        }
+        // for (const participant of data.participants) {
+        //     await prisma.participant.create({
+        //         data: participant,
+        //     });
+        // }
+
+        // for (const course of data.courses) {
+        //     await prisma.course.create({
+        //         data: course,
+        //     });
+        // }
+
+        // for (const participantOnCourse of data.participantsOnCourses) {
+        //     await prisma.participantOnCourse.create({
+        //         data: participantOnCourse,
+        //     });
+        // }
+
+        console.log("Database seeding from JSON completed successfully.");
+    } catch (error) {
+        console.error("Error seeding database from JSON:", error);
+    }
+}
 async function seedDatabase() {
     try {
         await prisma.participant.deleteMany()
         await prisma.course.deleteMany()
-        
-        for (let index = 0; index < 500; index++) {
-            await prisma.participant.create({
-                data: {
-                    email: faker.internet.email(),
-                    birthDate: faker.date.birthdate().toISOString(),
-                    firstName: faker.person.firstName(),
-                    firstSurname: faker.person.lastName(),
-                    secondSurname: faker.person.lastName(),
-                    hasWhatsApp: faker.helpers.arrayElement([YesOrNo.Yes, YesOrNo.No]),
-                    phoneNumber: faker.phone.number(),
-                    identification: faker.string.uuid(),
-                    typeIdentification: faker.helpers.arrayElement([TypeIdentification.DIMEX, TypeIdentification.Nacional]),
-                    grade: faker.helpers.arrayElement([Grade.Primaria_Completa, Grade.Primaria_Incompleta, Grade.Secundaria_Completa, Grade.Secundaria_Incompleta, Grade.Sin_Estudio, Grade.Universidad_Completa, Grade.Universidad_Incompleta])
-                }
-            })
-        }
-        // Array.from({ length: 500 }).forEach(async (_, index) => {
-        for (let index = 0; index < 500; index++) {
-            await prisma.course.create({
-                data: {
-                    name: faker.lorem.words(),
-                    description: faker.lorem.paragraph(),
-                    initialDate: faker.date.past().toISOString(),
-                    finalDate: faker.date.future().toISOString(),
-                    courseNumber: faker.string.uuid(),
-                    needMedicalReport: faker.helpers.arrayElement([YesOrNo.Yes, YesOrNo.No]),
-                    professor: faker.person.fullName(),
-                    quota: faker.helpers.rangeToNumber({ max: 20, min: 1 }),
-                    location: faker.location.city(),
-                    state: faker.helpers.arrayElement([State.Active, State.Inactive]),
-                }
-            })
-        }
-        // })
-        // Array.from({ length: 500 }).forEach(async (_, index) => {
-        for (let index = 0; index < 100; index++) {
-            await prisma.participantOnCourse.create({
-                data: {
-                    participantId: faker.helpers.rangeToNumber({ max: 500, min: 1 }),
-                    courseId: faker.helpers.rangeToNumber({ max: 500, min: 1 }),
-                    state: faker.helpers.arrayElement([StateParticipantOnCourse.Finished, StateParticipantOnCourse.Registered, StateParticipantOnCourse.Retired]),
-                }
-            })
-        }
+
+        // for (let index = 0; index < 500; index++) {
+        //     await prisma.participant.create({
+        //         data: {
+        //             email: faker.internet.email(),
+        //             birthDate: faker.date.birthdate().toISOString(),
+        //             firstName: faker.person.firstName(),
+        //             firstSurname: faker.person.lastName(),
+        //             secondSurname: faker.person.lastName(),
+        //             hasWhatsApp: faker.helpers.arrayElement([YesOrNo.Yes, YesOrNo.No]),
+        //             phoneNumber: faker.phone.number(),
+        //             identification: faker.string.uuid(),
+        //             typeIdentification: faker.helpers.arrayElement([TypeIdentification.DIMEX, TypeIdentification.Nacional]),
+        //             grade: faker.helpers.arrayElement([Grade.Primaria_Completa, Grade.Primaria_Incompleta, Grade.Secundaria_Completa, Grade.Secundaria_Incompleta, Grade.Sin_Estudio, Grade.Universidad_Completa, Grade.Universidad_Incompleta])
+        //         }
+        //     })
+        // }
+        // // Array.from({ length: 500 }).forEach(async (_, index) => {
+        // for (let index = 0; index < 500; index++) {
+        //     await prisma.course.create({
+        //         data: {
+        //             name: faker.lorem.words(),
+        //             description: faker.lorem.paragraph(),
+        //             initialDate: faker.date.past().toISOString(),
+        //             finalDate: faker.date.future().toISOString(),
+        //             courseNumber: faker.string.uuid(),
+        //             needMedicalReport: faker.helpers.arrayElement([YesOrNo.Yes, YesOrNo.No]),
+        //             professor: faker.person.fullName(),
+        //             quota: faker.helpers.rangeToNumber({ max: 20, min: 1 }),
+        //             location: faker.location.city(),
+        //             state: faker.helpers.arrayElement([State.Active, State.Inactive]),
+        //         }
+        //     })
+        // }
+        // // })
+        // // Array.from({ length: 500 }).forEach(async (_, index) => {
+        // for (let index = 0; index < 100; index++) {
+        //     await prisma.participantOnCourse.create({
+        //         data: {
+        //             participantId: faker.helpers.rangeToNumber({ max: 500, min: 1 }),
+        //             courseId: faker.helpers.rangeToNumber({ max: 500, min: 1 }),
+        //             state: faker.helpers.arrayElement([StateParticipantOnCourse.Finished, StateParticipantOnCourse.Registered, StateParticipantOnCourse.Retired]),
+        //         }
+        //     })
+        // }
 
         // })
         console.log("Database seeding completed successfully.");
@@ -60,4 +112,5 @@ async function seedDatabase() {
     }
 }
 
-seedDatabase();
+// seedDatabase();
+seed()
